2bc36e63ef902194444cef4fbba97f6fb5e9373a
Vadim Kushir <vadim.kushnir@logity.tech>
2024-12-06 12:25:22 +0300

    feat: implemented the rating counter component

---
+++ src/components/RatingCounter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="rating-counter">
+    <button class="rating-counter__button" @click="ratingChange(-1)">-</button>
+    <span class="rating-counter__text">{{ rating }}</span>
+    <button class="rating-counter__button" @click="ratingChange(1)">+</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const rating = ref(5)
+const ratingChange = (amount) => {
+  rating.value += amount
+}
+</script>
+
+<style scoped lang="scss">
+$height: 24px;
+$padding: 2px;
+$border-radius: 4px;
+
+.rating-counter {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: $height;
+  background-color: #eeeeee;
+  border-radius: $border-radius;
+  padding: $padding;
+
+  &__button {
+    height: calc($height - 2 * $padding);
+    width: calc($height - 2 * $padding);
+    border-radius: $border-radius;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__text {
+    font-weight: 500;
+  }
+}
+</style>
